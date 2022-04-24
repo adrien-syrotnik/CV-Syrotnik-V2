d01d17c2dde1bb31de0193ff3d640f5d8176a1fb
Adrien <adrien.syrotnik@gmail.com>
2022-04-24 14:04:20 +0200

    add score infos

--- src/app/_components/speedrun/game/category/game-category.component.ts
+++ src/app/_components/speedrun/game/category/game-category.component.ts
@@ -1,8 +1,10 @@
 import { Component, EventEmitter, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { GameService } from 'src/app/_services/api/game.service';
+import { ScoreInfoModalComponent } from '../score-info-modal/add-score-modal/score-info-modal.component';
 
 
 @Component({
@@ -18,9 +20,12 @@ export class GameCategoryComponent implements OnInit, OnDestroy {
 
     private sub! : Subscription;
 
+    public loading = false;
+
     public scores : any[] = [];
 
-    constructor(private gameService: GameService) {
+    constructor(private gameService: GameService,
+        private modalService: NgbModal) {
 
         
     
@@ -34,9 +39,17 @@ export class GameCategoryComponent implements OnInit, OnDestroy {
         
     }
 
+    openScoreInfoModal(scoreId : string) {
+        const modalRef = this.modalService.open(ScoreInfoModalComponent);
+        (modalRef.componentInstance as ScoreInfoModalComponent).gameInfos = this.gameInfo;
+        (modalRef.componentInstance as ScoreInfoModalComponent).scoreId = scoreId;
+    }
+
     getScores() {
+        this.loading = true;
         this.gameService.getGameCategoryScores(this.gameInfo.id, this.categoryId).subscribe((data: any) => {
             this.scores = data.data;
+            this.loading = false;
         });
     }
 
